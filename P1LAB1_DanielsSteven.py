d72c31842cee835c44ccd980cb98f3450bdb1c85
Danny9dime <danielss8072@student.faytechcc.edu>
2024-06-22 19:53:26 -0400

    FIRST SUBMISSION

---
+++ P1LAB1_DanielsSteven.py
@@ -0,0 +1,8 @@
+#StevenDaniels
+#6/23/2024
+#P1LAB1
+#Introduction to Python
+
+print("Enter your first name: Cynthia")
+print("Enter your last name: Cupcakes")
+print("Hello Cynthia Cupcakes! Welcome to CTI-110")
