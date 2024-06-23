f734b0db2df813341eb629fcdf4ed25667734cc1
Danny9dime <danielss8072@student.faytechcc.edu>
2024-06-23 17:34:54 -0400

    Add files via upload

---
+++ P1HW1_DanielsSteven.py
@@ -0,0 +1,26 @@
+#StevenDaniels
+#6/23/2024
+#P1HW1
+#Calculating Exponents
+
+print('-----Calculating Exponents-----')
+base = int (input('Enter an integer as the base value:'))
+print()
+exp = int (input('Enter an integer as the exponent:'))
+print()
+cal = base ** exp
+print(base , 'raised to the power of' , exp , 'is' , cal , '!!')
+
+print('-----Addition and Subtraction-----')
+start = int (input('Enter an starting integer: '))
+print()
+add = int (input('Enter an integer to add: '))
+print()
+sub = int (input('Enter an integer to subtract: '))
+print()
+
+cal2 = start + add - sub
+print(start, "+", add , "-" , sub , "is equal to" , cal2)
+
+
+      
